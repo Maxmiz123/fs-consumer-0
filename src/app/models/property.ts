bdfc3707fa05e6f6127eec9180965282911fb90a
Max Misrahi <maxmisrahi@Maxs-Air.lan>
2019-05-30 12:18:46 +0100

    new changes

---
+++ src/app/models/property.ts
@@ -0,0 +1,18 @@
+export class Property {
+    id: number;
+    name: string;
+    city: string;
+    cost: string;
+    rating: string;
+    date: string;
+
+
+    constructor() {
+        this.id = 0;
+        this.name = "";
+        this.city = "";
+        this.cost = "";
+        this.rating = "";
+        this.date = "";
+    }
+}
--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
-import { Payment } from '../models /payment'
+import { Payment } from '../models/payment'
 
 @Component({ 
   selector: 'app-tab5',
